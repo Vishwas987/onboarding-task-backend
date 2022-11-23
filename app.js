1206fe2ab29a0b38871a211a17b045f36bfaace5
Vishwas987 <vishwas.bhaskar@comprotechnologies.com>
2022-11-23 10:21:53 +0530

    fixed CORS policy error

--- app.js
+++ app.js
@@ -8,6 +8,17 @@ const port = process.env.PORT || 5000;
 
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
+
+app.use(function (req, res, next) {
+
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8080');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    next();
+});
+
 app.use('/api/v1/tasks', tasks);
 
 app.get('/', (req, res) => {
